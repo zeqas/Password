08e6d567066a45c9f09094b3bb284945803e4354
Casper Hsu <casperi52400@gmail.com>
2021-03-08 21:36:17 +0800

    For Real

--- password.py
+++ password.py
@@ -1,12 +1,14 @@
 x = 3
 password = 'a123456'
-while True:
+while x > 0 :
+    x = x - 1
     pwd = input('請輸入密碼')
     if pwd == password :
         print('登入成功!')
         break
     elif pwd != password :
-        x = x - 1 
-        print('密碼錯誤! 還有', x, '次機會')
+        if x != 0:
+            print('密碼錯誤! 還有', x, '次機會')
         if x == 0:
+            print('沒機會了!')
             break
